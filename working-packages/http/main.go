833a524f62ea744495c72e1e6149d075bf4da9bf
Euller Cristian <eullercdr@gmail.com>
2024-05-12 17:33:46 -0300

    feat: simple http request

---
+++ working-packages/http/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	request, err := http.Get("https://www.google.com")
+	if err != nil {
+		panic(err)
+	}
+	res, err := io.ReadAll(request.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(res))
+	request.Body.Close()
+}
