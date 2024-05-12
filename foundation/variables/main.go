d831ae538d72c81324e4093126126a3e8b057041
Euller Cristian <eullercdr@gmail.com>
2024-05-12 10:51:00 -0300

    feat: variables

---
+++ foundation/variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+var (
+	a string
+	b bool    = true
+	c int     = 10
+	d string  = "Euller"
+	e float64 = 1.2
+)
+
+func main() {
+	a = "Hello, World!"
+	b = true
+	f := 10 //only in first declaration
+	println(f)
+	println(a)
+	println(b)
+}
