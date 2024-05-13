db5866365430192ec6d4a268f288502ae22b397f
Euller Cristian <eullercdr@gmail.com>
2024-05-13 08:48:00 -0300

    feat: customize requests

---
+++ http-client-customize-requests/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", "http://httpbin.org/get", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Accept", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
