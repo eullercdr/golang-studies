48d9dd090d570dfd24ee67d5e7422f23368272f2
Euller Cristian <eullercdr@gmail.com>
2024-05-19 15:48:32 -0300

    feat: test using testify

---
+++ testing/2/tax.go
@@ -0,0 +1,33 @@
+package tax
+
+import (
+	"errors"
+	"time"
+)
+
+func CalculateTax(amount float64) (float64, error) {
+	if amount <= 0 {
+		return 0, errors.New("amount must be greater than 0")
+	}
+	if amount >= 1000 && amount < 20000 {
+		return 10.0, nil
+	}
+	if amount >= 20000 {
+		return 20.0, nil
+	}
+	if amount < 1000 {
+		return amount * 0.05, nil
+	}
+	return 5.0, nil
+}
+
+func CalculateTax2(amount float64) float64 {
+	time.Sleep(1 * time.Millisecond)
+	if amount == 0 {
+		return 0
+	}
+	if amount < 1000 {
+		return amount * 0.05
+	}
+	return 5.0
+}
