614b2f5f45f1e1ae0ba1f437166858203401863f
Euller Cristian <eullercdr@gmail.com>
2024-05-19 11:43:41 -0300

    feat: work with benchmarking

--- testing/1/tax.go
+++ testing/1/tax.go
@@ -1,6 +1,19 @@
 package tax
 
+import "time"
+
 func CalculateTax(amount float64) float64 {
+	if amount == 0 {
+		return 0
+	}
+	if amount < 1000 {
+		return amount * 0.05
+	}
+	return 5.0
+}
+
+func CalculateTax2(amount float64) float64 {
+	time.Sleep(1 * time.Millisecond)
 	if amount == 0 {
 		return 0
 	}
--- testing/1/tax_test.go
+++ testing/1/tax_test.go
@@ -2,6 +2,8 @@ package tax
 
 import "testing"
 
+// go test -bench=.
+// go test -html=coverage.out
 func TestCalculateTax(t *testing.T) {
 	amount := 500.0
 	expected := 25.0
@@ -25,5 +27,20 @@ func TestCalculateTaxBatch(t *testing.T) {
 		if result != test.expected {
 			t.Errorf("Expected %f, got %f", test.expected, result)
 		}
+	}
+}
+
+// go test -bench=.
+// go test -bench=. -run=^#
+func BenchmarkCalculateTax(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax(500.0)
+	}
+}
+
+// go test -bench=. -run=^# -count=10 -benchtime=3s
+func BenchmarkCalculateTax2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax2(500.0)
 	}
 }
