16d6d4479597e6d947a0d9b35ed54c3e30af7408
Euller Cristian <eullercdr@gmail.com>
2024-05-21 08:17:07 -0300

    feat: find api crud of products

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/eullercdr/go/api/internal/entity"
 	"github.com/eullercdr/go/api/internal/infra/database"
 	"github.com/eullercdr/go/api/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -23,6 +25,13 @@ func main() {
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 	productDb := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDb)
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8080", nil)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Get("/products", productHandler.GetProducts)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	http.ListenAndServe(":8080", r)
 }
--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module github.com/eullercdr/go/api
 go 1.19
 
 require (
+	github.com/go-chi/chi/v5 v5.0.12
 	github.com/go-chi/jwtauth v1.2.0
 	github.com/google/uuid v1.4.0
 	github.com/spf13/viper v1.18.2
