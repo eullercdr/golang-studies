7afc44be97f2e923b0febb4f538201f952b00bbb
Euller Cristian <eullercdr@gmail.com>
2024-05-22 11:00:33 -0300

    feat: add swagger

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -5,16 +5,35 @@ import (
 	"net/http"
 
 	configs "github.com/eullercdr/go/api"
+	_ "github.com/eullercdr/go/api/docs"
 	"github.com/eullercdr/go/api/internal/entity"
 	"github.com/eullercdr/go/api/internal/infra/database"
 	"github.com/eullercdr/go/api/internal/infra/webserver/handlers"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+// @title           Go Expert API Example
+// @version         1.0
+// @description     Product API with auhtentication
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Euller Cristian
+// @contact.url    http://www.fullcycle.com.br
+// @contact.email  euller@fullcycle.com.br
+
+// @license.name   Full Cycle License
+// @license.url    http://www.fullcycle.com.br
+
+// @host      localhost:8080
+// @BasePath  /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
@@ -48,6 +67,8 @@ func main() {
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8080/docs/doc.json")))
 
 	http.ListenAndServe(":8080", r)
 }
